ca7c4670265deedb35157a35724d51bdf795ec8e
mhplong <mhplong@gmail.com>
2022-02-06 16:15:55 -0600

    update branch

---
+++ Assets/Scripts/ServerButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Mirror.Discovery
+{
+    public class ServerButton : MonoBehaviour
+    {
+        public long connectionID;
+        public NetworkDiscoveryVRHUD hud;
+
+        public void Connect()
+        {
+            hud.Connect(connectionID);
+        }
+    }
+}
